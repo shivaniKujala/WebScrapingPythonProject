4eb5bf018b9ced41113f94034b74ff6912dc6a76
shivani <shivanikujala@gmail.com>
2022-08-02 15:15:17 +0530

    Storing tables

--- store_quote.py
+++ store_quote.py
@@ -11,29 +11,31 @@ c = connection.cursor()
 
 quotes = data['quotes']
 authors = data['authors']
+#Drop table if exists
+c.execute("DROP TABLE IF EXISTS author_table")
+
 
 #Creating Author table using authors data
-#c.execute('''CREATE TABLE author_table(id INTEGER NOT NULL PRIMARY KEY,name VARCHAR(200),born VARCHAR(200), reference)''')
+c.execute('''CREATE TABLE author_table(id INTEGER NOT NULL,name VARCHAR(200))''')
 
 #Inserting data about authors by iterating over the authors list
 for each_author in authors:
     name = each_author['name']
-    born = each_author['born']
-    reference = each_author['reference']
-    #c.execute('''INSERT INTO author_table VALUES(?,?,?)''',(name,born,reference))
+    #born = each_author['born']
+    #reference = each_author['reference']
+    c.execute('''INSERT INTO author_table VALUES(?,?)''',(id,name))
 
 
 #Creating Quotes Table using data in quotes list
-#c.execute('''CREATE TABLE quotes_table(id INTEGER NOT NULL PRIMARY KEY,quote VARCHAR(200), tags VARCHAR(200),author_id INTEGER
-# FOREIGN KEY (author_id) REFERENCES author_table(id) ON DELETE CASCADE)''')
-for each_quote in quotes:
-    quote = each_quote['quote']
-    tags = each_quote['tags']
+#c.execute('''CREATE TABLE quotes_table(id INTEGER NOT NULL PRIMARY KEY,quote VARCHAR(200), tags VARCHAR(200),author_id INTEGER FOREIGN KEY (author_id) REFERENCES author_table(id) ON DELETE CASCADE)''')
+#for each_quote in quotes:
+    #quote = each_quote['quote']
+    #tags = each_quote['tags']
     
 
     #c.execute('''INSERT INTO quotes_table VALUES(?,?,?)'''(quote,tags))
 
 
-
+results = c.fetchall()
 
 c.close()
